b6bd90df4b6d9c96163e7e8de62a6d9c73d6a761
wilmer990413 <wilmer.castro@unillanos.edu.co>
2022-05-01 20:32:22 -0500

    Actualización Script Database

--- script/Untitled (1).sql
+++ script/Untitled (1).sql
@@ -1,32 +1,31 @@
 CREATE TABLE "estudiante" (
-  "id_est" serial PRIMARY KEY,
-  "identif_est" bigint,
-  "nombre_est" varchar,
-  "apellido_est" varchar,
-  "correo_institucional_est" varchar,
-  "telefono_est" bigint
+  "id_est" serial PRIMARY KEY NOT NULL,
+  "identif_est" bigint NOT NULL,
+  "nombre_est" varchar NOT NULL,
+  "apellido_est" varchar NOT NULL,
+  "correo_institucional_est" varchar NOT NULL,
+  "telefono_est" bigint NOT NULL
 );
 
 CREATE TABLE "docente" (
-  "id_doc" serial PRIMARY KEY,
-  "identif_doc" bigint,
-  "nombre_doc" varchar,
-  "apellido_doc" varchar,
-  "nivel_educativo" varchar,
-  "correo_institucional_doc" varchar
+  "identif_doc" bigint PRIMARY KEY NOT NULL,
+  "nombre_doc" varchar NOT NULL,
+  "apellido_doc" varchar NOT NULL,
+  "nivel_educativo" varchar NOT NULL,
+  "correo_institucional_doc" varchar NOT NULL
 );
 
 CREATE TABLE "proyecto" (
-  "id_pro" serial PRIMARY KEY,
-  "nombre_pro" varchar,
-  "fecha_limite" date,
+  "id_pro" serial PRIMARY KEY NOT NULL,
+  "nombre_pro" varchar NOT NULL,
+  "fecha_limite" date NOT NULL,
   "archivo_pro" bytea,
-  "estado_pro" boolean,
+  "estado_pro" varchar,
   "retroalimentacion_pro" varchar,
-  "id_est_fk" bigint,
-  "id_doc_fk" bigint
+  "id_est_fk" bigint NOT NULL,
+  "identif_doc_fk" bigint NOT NULL
 );
 
-ALTER TABLE "proyecto" ADD FOREIGN KEY ("id_doc_fk") REFERENCES "docente" ("id_doc");
+ALTER TABLE "proyecto" ADD FOREIGN KEY ("identif_doc_fk") REFERENCES "docente" ("identif_doc");
 
 ALTER TABLE "proyecto" ADD FOREIGN KEY ("id_est_fk") REFERENCES "estudiante" ("id_est");
